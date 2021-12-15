5d4ceb242bb6eb1d40d049724c4caf9c0ffcde99
Yanniku <borisov-yan@list.ru>
2021-12-15 13:22:30 +0300

    loaded project

--- starter-bot/src/main/java/ru/croccode/hypernull/bot/StarterBot.java
+++ starter-bot/src/main/java/ru/croccode/hypernull/bot/StarterBot.java
@@ -3,10 +3,15 @@ package ru.croccode.hypernull.bot;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 import ru.croccode.hypernull.domain.MatchMode;
 import ru.croccode.hypernull.geometry.Offset;
+import ru.croccode.hypernull.geometry.Point;
+import ru.croccode.hypernull.geometry.Size;
 import ru.croccode.hypernull.io.SocketSession;
 import ru.croccode.hypernull.message.Hello;
 import ru.croccode.hypernull.message.MatchOver;
@@ -25,39 +30,69 @@ public class StarterBot implements Bot {
 
 	private int moveCounter = 0;
 
+	private Size map_size;
+
+	private boolean[][] map;
+
+	private Integer id;
+
+	private Point current_place;
+
 	public StarterBot(MatchMode mode) {
 		this.mode = mode;
 	}
+
 
 	@Override
 	public Register onHello(Hello hello) {
 		Register register = new Register();
 		register.setMode(mode);
-		register.setBotName("starter-bot");
+		register.setBotName("Yan-Borisov");
 		return register;
 	}
 
 	@Override
 	public void onMatchStarted(MatchStarted matchStarted) {
+		id = matchStarted.getYourId();
+		map_size = matchStarted.getMapSize();
+		map = new boolean[matchStarted.getMapSize().width()][matchStarted.getMapSize().height()];
 	}
 
 	@Override
 	public Move onUpdate(Update update) {
+		current_place = update.getBots().get(id);
+		Set<Point> blocks = update.getBlocks();
+		for(Point block : blocks){
+			map[block.x()][block.y()] = true;
+		}
+		Set<Point> coins = update.getCoins();
 		if (moveOffset == null || moveCounter > 5 + rnd.nextInt(5)) {
 			moveOffset = new Offset(
 					rnd.nextInt(3) - 1,
-					rnd.nextInt(3) - 1
-			);
+					rnd.nextInt(3) - 1);
 			moveCounter = 0;
 		}
-		moveCounter++;
+		if(map[current_place.apply(moveOffset, map_size).x()][current_place.apply(moveOffset, map_size).y()]){
+			moveOffset = new Offset(
+					-rnd.nextInt(3) - 1,
+					-rnd.nextInt(3) - 1);
+		}
+		if(coins != null) {
+			for (Point coin : coins) {
+				moveOffset = new Offset(coin.x() - current_place.x(), coin.y() - current_place.y());
+			}
+		}
+		moveOffset = new Offset(
+				rnd.nextInt(3) - 1,
+				rnd.nextInt(3) - 1);
 		Move move = new Move();
 		move.setOffset(moveOffset);
+		System.out.println(Arrays.deepToString(map));
 		return move;
 	}
-
 	@Override
 	public void onMatchOver(MatchOver matchOver) {
+		System.out.println("Match ended");
 	}
 
 	public static void main(String[] args) throws IOException {
